fcd834ceb5e8b50de1d6f6e4083bc3faa10c8779
Luís Farias <luisgmfarias@gmail.com>
2023-01-11 23:09:16 -0300

    fix gupy scraper

--- jobs/management/commands/scrape_gupy.py
+++ jobs/management/commands/scrape_gupy.py
@@ -1,6 +1,7 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 import re
+import json
 
 from jobs.management.commands._private import fix_workplace_name, update_get_company, save_job
 
@@ -8,25 +9,25 @@ from jobs.management.commands._private import fix_workplace_name, update_get_com
 class Gupy():
     help = "collect jobs"
 
-    def get_company_info(self, soup, company_url, company_name):
+    def get_company_info(self, data, company_url, company_name):
 
-        logo = soup.find("div", {"class", "header__logo"}).find("img")["src"]
-        logo_bin = urlopen(logo).read()
+        logo_src = data.get("urlLogo")
+        logo_bin = requests.get(logo_src).content
 
-        try:
-            social_medias = soup.find("ul", {"class", "description__social"})
-            urls = [
-                li.find("a", href=True)["href"] for li in social_medias.find_all("li")
-            ]
-        except Exception:
-            urls = [None, " ", " "]
+        urls = [section for section in data.get(
+            'sections') if section['type'] == "social_links_section"]
 
-        website = urls[0]
-        linkedin = urls[1] if "linkedin" in urls[1] else None
-        glassdoor = urls[-1] if "glassdoor" in urls[-1] else None
+        website = urls[0].get('urlSite') if len(
+            urls) > 0 and urls[0].get('urlSite') else None
+
+        linkedin = urls[0].get('urlLinkedin') if len(
+            urls) > 0 and urls[0].get('urlLinkedin') else None
+
+        glassdoor = urls[0].get('urlGlassdoor') if len(
+            urls) > 0 and urls[0].get('urlGlassdoor') else None
 
         company = update_get_company(company_url=company_url, company_name=company_name,
-                               website=website, glassdoor=glassdoor, linkedin=linkedin, logo_bin=logo_bin)
+                                     website=website, glassdoor=glassdoor, linkedin=linkedin, logo_bin=logo_bin)
         return company
 
     def get_job(self, company, terms, exceptions):
@@ -35,47 +36,49 @@ class Gupy():
 
         print(company["website"])
 
-        website = urlopen(company["website"])
+        req = requests.get(company["website"])
 
-        bs = BeautifulSoup(website, "html.parser")
+        bs = BeautifulSoup(req.content, "html.parser")
+
+        script = bs.find("script", {"id": "__NEXT_DATA__"}).text
+
+        page_data = json.loads(script)
+
+        data = page_data.get('props').get('pageProps').get('careerPage')
+        
 
         try:
             c = self.get_company_info(
-                soup=bs,
+                data=page_data.get('props').get('pageProps').get('careerPage'),
                 company_url=company["website"],
                 company_name=company["company_name"],
             )
         except Exception as e:
             print(e)
+        
+        job_list = page_data.get('props').get('pageProps').get('jobs')
 
-        job_list = bs.find("div", {"class": "job-list jobs-to-filter"})
-        table = job_list.find("table")
-        rows = table.find_all("tr")
+        section_list = bs.find("ul", {"aria-label": "Lista de Vagas"})
+        urls_list = [li.find("a", href=True)["href"] for li in section_list]
 
-        for row in rows:
-
-            data = row.find("h4")
-            role = data.find("span").text
+        for job, url in zip(job_list, urls_list):
+            role = job.get("title")
             roleSplited = re.sub('[,.;@#?!/\|&$)(-]+\|*', ' ', role).lower().split()
+            remote_status = job.get("workplace").get("remoteWorking")
+            workplace = job.get("workplace").get("address").get("city")
 
             for term in terms:
                 if all(elem in roleSplited for elem in term.lower().split()):
                     if not any(e in role for e in exceptions):
-                        a = row.find("a", href=True)
-                        workplace = row["data-workplace"]
                         workplace_parsed = fix_workplace_name(
                             workplace) if workplace != '' else ''
-                        remote = row["data-remote"]
-
-                        remote_status = remote.lower() in [
-                            "true", "1", "t", "y", "yes", "True"]
                         try:
                             save_job(
-                                title=role, url=company["website"] + a["href"], remote=remote_status, location=workplace_parsed, company=c)
+                                title=role, url=company["website"] + url, remote=remote_status, location=workplace_parsed, company=c)
 
                         except Exception as e:
                             print(e)
 
-                        job_urls_list.append(company["website"] + a["href"])
+                        job_urls_list.append(company["website"] + url)
 
         return job_urls_list
